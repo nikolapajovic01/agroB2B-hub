8e0766ad95d7ae14963765ba42c8e4cb55753765
Nikola Pajovic <npajovic11922rn@raf.rs>
2025-06-24 02:23:27 +0200

    Breadcrumb: change 'Home' to 'Back' and use navigate(-1)

--- ui/src/components/Breadcrumbs/Breadcrumb.tsx
+++ ui/src/components/Breadcrumbs/Breadcrumb.tsx
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 interface BreadcrumbProps {
   pageName: string;
 }
 const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
+  const navigate = useNavigate();
   return (
     <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
       <h2 className="text-title-md2 font-semibold text-black dark:text-white">
@@ -12,9 +13,12 @@ const Breadcrumb = ({ pageName }: BreadcrumbProps) => {
       <nav>
         <ol className="flex items-center gap-2">
           <li>
-            <Link className="font-medium" to="/">
-              Početna /
-            </Link>
+            <span
+              className="font-medium cursor-pointer"
+              onClick={() => navigate(-1)}
+            >
+                Prethodna stranica /
+            </span>
           </li>
           <li className="font-medium text-primary">{pageName}</li>
         </ol>
