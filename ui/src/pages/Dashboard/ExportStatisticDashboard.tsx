273f68531b22cf85367ed4cd271e8184c38a86c1
Nikola Pajovic <npajovic11922rn@raf.rs>
2025-07-14 15:26:40 +0200

    trying

--- ui/src/pages/Dashboard/ExportStatisticDashboard.tsx
+++ ui/src/pages/Dashboard/ExportStatisticDashboard.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import CardDataStats from '../../components/CardDataStats';
 import DefaultLayout from '../../layout/DefaultLayout';
-import ExportMainChart from "../../components/Marketplace/ExportMainChart";
-import MapExportByCountry from "../../components/Marketplace/Map";
-import ExportByCountryContent from "../../components/Marketplace/ExportByCountryContent";
+// import ExportMainChart from "../../components/Marketplace/ExportMainChart";
+// import MapExportByCountry from "../../components/Marketplace/Map";
+// import ExportByCountryContent from "../../components/Marketplace/ExportByCountryContent";
 
 const ExportStatisticDashboard: React.FC = () => {
   return (
@@ -104,11 +104,13 @@ const ExportStatisticDashboard: React.FC = () => {
       </div>
 
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        {/*
         <ExportMainChart/>
         <MapExportByCountry/>
         <div className="col-span-12 xl:col-span-6">
           <ExportByCountryContent/>
         </div>
+        */}
       </div>
 
     </DefaultLayout>
