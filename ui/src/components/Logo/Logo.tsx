e0df884ac7bb69dfabf00b7e4ff8549babd3e5b0
Nikola Pajovic <npajovic11922rn@raf.rs>
2025-07-14 15:00:58 +0200

    favicon is changed

--- ui/src/components/Logo/Logo.tsx
+++ ui/src/components/Logo/Logo.tsx
@@ -1,7 +1,17 @@
+import { Link } from 'react-router-dom';
 import logoAgroB2B from '../../assets/images/logoAgroB2B.png';
 
-const Logo = () => (
-  <img src={logoAgroB2B} alt="AgroB2B Logo" className="mx-auto mb-10 h-16 w-16" />
-);
+const Logo = () => {
+  return (
+    <Link className="mb-5.5 inline-block" to="/">
+      <div className="flex items-center">
+        <img src={logoAgroB2B} alt="AgroB2B Logo" className="h-12 w-12" />
+        <span className="ml-2 text-2xl font-bold text-black dark:text-white">
+          AgroB2B Hub
+        </span>
+      </div>
+    </Link>
+  );
+};
 
 export default Logo; 
--- ui/src/components/Sidebar/index.tsx
+++ ui/src/components/Sidebar/index.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import SidebarLinkGroup from './SidebarLinkGroup';
+import Logo from '../Logo/Logo';
 
 interface SidebarProps {
   sidebarOpen: boolean;
@@ -658,31 +659,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <NavLink to={userType === 'company' ? '/dashboard' : '/buy-offers'}>
-          <div className="flex items-center">
-            <div className="rounded-lg bg-primary p-2">
-              <svg
-                className="h-8 w-8 text-white"
-                viewBox="0 0 24 24"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M7 21C5.89543 21 5 20.1046 5 19V5C5 3.89543 5.89543 3 7 3H17C18.1046 3 19 3.89543 19 5V19C19 20.1046 18.1046 21 17 21H7Z"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                />
-                <path
-                  d="M9 7L15 7"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                />
-              </svg>
-            </div>
-            <span className="ml-2 text-2xl font-bold text-white">
-              AgroB2B Hub
-            </span>
-          </div>
+          <Logo />
         </NavLink>
 
         <button
